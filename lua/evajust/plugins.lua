99cf8791d3e5c2c4cac9fc566ae27c0a48e77ddc
EC2 Default User <ec2-user@ip-10-0-0-58.ec2.internal>
2023-12-26 05:52:05 +0000

    Stripping down plugins to enable one by one

--- lua/evajust/plugins.lua
+++ lua/evajust/plugins.lua
@@ -18,24 +18,26 @@ vim.opt.rtp:prepend(lazypath)
 local plugins = {
 	{ 'rose-pine/neovim', name = 'rose-pine' },
 	'nvim-treesitter/nvim-treesitter',
-	'nvim-treesitter/playground',
 	'theprimeagen/harpoon',
 	'mbbill/undotree',
+    -- Git integration
 	'tpope/vim-fugitive',
-        {
+    {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 		"neovim/nvim-lspconfig",
 	},
 	-- completion
-	{
-		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-nvim-lsp",
-		"L3MON4D3/LuaSnip",
-		-- snippets
-		"saadparwaiz1/cmp_luasnip",
-		"rafamadriz/friendly-snippets"
-	},
+	--{
+	--	"hrsh7th/nvim-cmp",
+	--	"hrsh7th/cmp-nvim-lsp",
+	--	"L3MON4D3/LuaSnip",
+    --},
+	-- snippets
+    --{
+	--	"saadparwaiz1/cmp_luasnip",
+	--	"rafamadriz/friendly-snippets"
+	--},
 	{
 		'nvim-telescope/telescope.nvim',
 		tag = '0.1.2',
